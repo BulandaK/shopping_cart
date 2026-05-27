7d33868c761282f2a8f2e6f18574c62455ff0533
BulandaK <eskimosl@wp.pl>
2026-05-27 17:28:47 +0200

    fix: fix table name

--- src/main/java/com/example/shopping_cart/model/Cart.java
+++ src/main/java/com/example/shopping_cart/model/Cart.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "cart")
+@Table(name = "carts")
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
